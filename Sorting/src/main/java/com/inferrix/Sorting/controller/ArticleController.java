8a42d55bae62b56357e98e04c8660c1828d87e87
aashish Bhagat <aashish2012@gmail.com>
2022-03-08 21:25:13 +0530

    final code

--- Sorting/src/main/java/com/inferrix/Sorting/controller/ArticleController.java
+++ Sorting/src/main/java/com/inferrix/Sorting/controller/ArticleController.java
@@ -1,12 +1,19 @@
 package com.inferrix.Sorting.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.inferrix.Sorting.model.Article;
 import com.inferrix.Sorting.service.ArticleService;
@@ -29,5 +36,7 @@ public class ArticleController {
 		List<Article> allArticles = service.findArticleWithSorting(field);
 		return allArticles;
 	}
+	
+
 
 }
